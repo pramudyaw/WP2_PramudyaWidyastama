74337f431d2565c4f1049ef1b9d89ca2194c5145
pramudyaw <15200088@bsi.ac.id>
2022-03-21 15:33:25 +0700

    Modifikasi

--- application/controllers/latihan1.php
+++ application/controllers/latihan1.php
@@ -5,4 +5,12 @@ class latihan1 extends CI_Controller
  {
  echo "Selamat Datang.. selamat belajar Web Programming";
  }
+
+ public function penjumlahan($n1, $n2)
+ {
+ $this->load->model('Model_latihan1');
+ $hasil = $this->Model_latihan1->jumlah($n1, $n2);
+ echo "Hasil Penjumlahan dari". $n1 ." + ". $n2 ." = "
+.$hasil;
+ }
 }
