b51fc8915565a00300a557a7cf6f5f91f1620d40
pramudyaw <15200088@bsi.ac.id>
2022-03-29 23:55:08 +0700

    ...

--- application/views/v_index.php
+++ application/views/v_index.php
@@ -1,15 +1,15 @@
 <section>
-            <h1><?php echo $judul ?></h1>
-            <p align='justify'>Pada pengertian codeigniter di atas
+ <h1><?php echo $judul ?></h1>
+ <p align='justify'>Pada pengertian codeigniter di atas
 tadi di jelaskan bahwa codeigniter menggunakan metode MVC. Apa itu
 MVC? Kita juga harus mengetahui apa itu MVC sebelum masuk dan lebih
 jauh dalam belajar codeigniter.</p>
-            <p>MVC adalah teknik atau konsep yang memisahkan
+ <p>MVC adalah teknik atau konsep yang memisahkan
 komponen utama menjadi tiga komponen yaitu model, view dan
 controller.</p>
 
-                <ol type=”a”>
-                <li>Model</li>
+ <ol type=”a”>
+ <li>Model</li>
 <p align='justify'>Model adalah kelas yang merepresentasikan atau
 memodelkan tipe data yang akan digunakan oleh aplikasi. Model juga
 dapat didefinisakn sebagai bagian penanganan yang berhubungan dengan
@@ -47,4 +47,4 @@ mendesign tampilann aplikasi, karena back-end developer yang
 menangani bagian controller dan modelnya. Jadi pembagian tugas pun
 menjadi mudah dan pengembangan aplikasi dapat di lakukan dengan
 cepat dan terstruktur.</p>
-        </section>
+ </section>
