626e5281c50c7b848e1529997791c6663adf85d0
pramudyaw <15200088@bsi.ac.id>
2022-03-30 13:04:41 +0700

    Tugas Pertemuan 3

--- application/controllers/Web.php
+++ application/controllers/Web.php
@@ -11,7 +11,7 @@ class Web extends CI_Controller{
  public function index(){
      $data ['judul'] = "Halaman Depan";
      $this->load->view('v_header',$data);
-     $this->load->view('v_index',$data);
+     $this->load->view('v_about',$data);
      $this->load->view('v_footer',$data); 
     }
 }
